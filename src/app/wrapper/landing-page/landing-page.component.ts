99bd277f154e63df96ab1ec6cc968b7d45b042c7
Bilal Mohammed <kurdo1ka@gmail.com>
2024-12-12 10:45:30 +0100

    last version before changing folders path

--- src/app/wrapper/landing-page/landing-page.component.ts
+++ src/app/wrapper/landing-page/landing-page.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './../../shared/nav-bar/nav-bar.component';
 import { AboutMeComponent } from './../about-me/about-me.component';
 import { MySkillsComponent } from '../my-skills/my-skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+
 
 
 
@@ -10,7 +12,7 @@ import { MySkillsComponent } from '../my-skills/my-skills.component';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, NavBarComponent, AboutMeComponent, MySkillsComponent],
+  imports: [CommonModule, NavBarComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
