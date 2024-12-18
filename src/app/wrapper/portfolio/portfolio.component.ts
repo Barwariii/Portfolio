f7f6c0fea92226fa71d41aa50edcf4acb23c14d3
Bilal Mohammed <kurdo1ka@gmail.com>
2024-12-18 18:26:27 +0100

    added new section and editing

--- src/app/wrapper/portfolio/portfolio.component.ts
+++ src/app/wrapper/portfolio/portfolio.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
 
+  projects = [
+    {
+      id: '1',
+      name: 'Jion',
+      Languages: 'JavaScript | HTML | CSS | Firebase',
+      description: 'Task Manager inspired by the Kanban System. Create and organize tasks suing drag and drop functions, assign users and categories.',
+      url: 'https://github.com/lbalz/Join-Kanban-Board',
+      project_number: '01/02',
+      image: './assets/img/portfolio-designs/join photo.png',
+      isReverse: false
+   },
+   {
+    id: '2',
+    name: 'El Pollo loco',
+    Languages: 'HTML | CSS | JavaScript',
+    description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen',
+    url: 'https://github.com/Barwariii/El-Pollo-Loco',
+    project_number: '02/02',
+    image: './assets/img/portfolio-designs/Pollo loco photo.png',
+    isReverse: true
+
+  },
+  ]
 }
--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -5,6 +5,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from "../shared/footer/footer.component";
+
 
 
 
@@ -15,7 +17,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 @Component({
   selector: 'app-wrapper',
   standalone: true,
-  imports: [CommonModule, NavBarComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [CommonModule, NavBarComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, FooterComponent, FooterComponent],
   templateUrl: './wrapper.component.html',
   styleUrl: './wrapper.component.scss'
 })
